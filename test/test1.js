3bda396947e39768110daa6148653f0cdb59397d
Ingi Kim <ikk5515@naver.com>
2021-09-14 03:27:58 +0900

    js

--- test/test1.js
+++ test/test1.js
@@ -1,4 +1,4 @@
-var button = document.getElementById('button');
+var button = document.getElementById('btn');
 	button.addEventListener('click', function() { alert('one') });
 	button.addEventListener('click', function() { alert('Two') });
 	button.addEventListener('click', function() { alert('Three') });
--- test1.js
+++ test1.js
@@ -1,6 +1,7 @@
 var sum=0;
      function f() {
          sum++;
+         console.log(sum);
          if(sum==10) clearInterval(id);
      }
 var id = setInterval("f()", 200);
